7506dc1c93d29910831f44d998dd5601e7a1d6d3
Alexandre Vitorino Martins de Lima <alexandrevmartinsdelima@gmail.com>
2023-10-02 21:17:46 -0300

    retornando ao caminho tradicional

--- src/utils/buttonsData.ts
+++ src/utils/buttonsData.ts
@@ -1,22 +1,22 @@
 export const buttonsData = [
   {
-    imageSrc: 'public/images/tripulacao-chopper.png',
+    imageSrc: 'src/assets/images/tripulacao-chopper.png',
     altText: 'Logo da tripulação do Chopper',
   },
   {
-    imageSrc: 'public/images/tripulacao-zoro.png',
+    imageSrc: 'src/assets/images/tripulacao-zoro.png',
     altText: 'Logo da tripulação do Zoro',
   },
   {
-    imageSrc: 'public/images/tripulacao-luffy.png',
+    imageSrc: 'src/assets/images/tripulacao-luffy.png',
     altText: 'Logo da tripulação do Luffy',
   },
   {
-    imageSrc: 'public/images/tripulacao-nami.png',
+    imageSrc: 'src/assets/images/tripulacao-nami.png',
     altText: 'Logo da tripulação da Nami',
   },
   {
-    imageSrc: 'public/images/tripulacao-sanji.png',
+    imageSrc: 'src/assets/images/tripulacao-sanji.png',
     altText: 'Logo da tripulação do Sanji',
   },
 ]
--- src/utils/charactersData.ts
+++ src/utils/charactersData.ts
@@ -1,35 +1,35 @@
 export const charactersData = [
   {
     name: 'Tony Tony Chopper',
-    imageSrc: 'public/images/personagem-tony-chopper.png',
+    imageSrc: 'src/assets/images/personagem-tony-chopper.png',
     alt: 'Imagem do Tony Tony Chopper',
     description:
       'Esta pequena rena ganhou a habilidade de mudar sua forma e de pensar como humanos após comer a fruta Hito Hito no Mi. Chopper é um valioso membro da tripulação dos Piratas do Chapéu de Palha, atuando como médico da tripulação.',
   },
   {
     name: 'Roronoa Zoro',
-    imageSrc: 'public/images/personagem-roronoa-zoro.png',
+    imageSrc: 'src/assets/images/personagem-roronoa-zoro.png',
     alt: 'Imagem do Roronoa Zoro',
     description:
       'Primeiro pirata (segundo membro se contarmos com Luffy) a se juntar à tripulação de Piratas do Chapéu de Palha, Zoro aceitou o convite de Luffy após o capitão salvar sua vida.',
   },
   {
     name: 'Monkey D. Luffy',
-    imageSrc: 'public/images/personagem-monkey-d-luffy.png',
+    imageSrc: 'src/assets/images/personagem-monkey-d-luffy.png',
     alt: 'Imagem do Monkey D. Luffy',
     description:
       '"Luffy do Chapéu de Palha", como ficou conhecido, é o protagonista do anime, e o fundador e capitão da tripulação Piratas do Chapéu de Palha. Desde muito jovem, tem como seu maior sonho um dia encontrar o lendário tesouro de Gol D. Roger, para se tornar o novo Rei dos Piratas.',
   },
   {
     name: 'Nami',
-    imageSrc: 'public/images/personagem-nami.png',
+    imageSrc: 'src/assets/images/personagem-nami.png',
     alt: 'Imagem da Nami',
     description:
       'Uma órfã de guerra, ainda criança Nami foi adotada por Bell-mère, uma mulher da Marinha. Enquanto crescia ao lado de sua irmã adotiva Nojiko, Nami já demonstrava sua paixão por desenhar mapas, sonhando em um dia fazer o mapa de todo o mundo.',
   },
   {
     name: 'Vinsmoke Sanji',
-    imageSrc: 'public/images/personagem-sanji.png',
+    imageSrc: 'src/assets/images/personagem-sanji.png',
     alt: 'Imagem do Vinsmoke Sanji',
     description:
       'Também conhecido como "Perna Negra" Sanji, este pirata foi o quinto a integrar a tripulação de Luffy. Suas ações como pirata lhe renderam a terceira maior recompensa entre os membros do Chapéu de Palha, além de atuar como cozinheiro oficial do grupo.',
